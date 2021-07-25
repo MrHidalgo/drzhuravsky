b4f9d2bb5448788ac2b2a9b655815e48abc88fe3
Andrii <Luckyhidalgo@gmail.com>
2021-07-25 17:09:27 +0300

    Common :::   - added header fixed animation & hover states - w\o adaptive;

--- src/js/_document/indexNative.js
+++ src/js/_document/indexNative.js
@@ -24,7 +24,7 @@
 
 		// lib
 		initHamburger();
-		// initHeaderFixed();
+		initHeaderFixed();
 		// ==========================================
 
 		// callback
--- src/js/_lib/headerFixed.js
+++ src/js/_lib/headerFixed.js
@@ -24,28 +24,24 @@ const initHeaderFixed = () => {
   
   $window.on('scroll', function (ev) {
     for(let i = 0; i < $element.length; i++) {
-    
-      if($window.scrollTop() > ($window.height() / 2.5)) {
-      
-        elHeight = $($element[i]).outerHeight();
-        dHeight = $document.height();
-        wHeight = $window.height();
-        wScrollCurrent = $window.scrollTop();
-        wScrollDiff = wScrollBefore - wScrollCurrent;
-        elTop = parseInt($($element[i]).css('top')) + wScrollDiff;
-      
-        if (wScrollCurrent <= 0) {
-          $($element[i]).css('top', 0).removeClass('is-fixed is-hide');
-        } else if (wScrollDiff > 0) {
-          $($element[i]).css('top', 0).removeClass('is-hide').addClass('is-fixed');
-        } else if (wScrollDiff < 0) {
-          if (wScrollCurrent + wHeight >= dHeight - elHeight) {
-            // $($element[i]).css('top', (elTop = wScrollCurrent + wHeight - dHeight) < 0 ? elTop : 0);
-          } else {
-            $($element[i]).css('top', -($element.outerHeight(true))).addClass('is-hide');
-          }
+  
+      elHeight = $($element[i]).outerHeight();
+      dHeight = $document.height();
+      wHeight = $window.height();
+      wScrollCurrent = $window.scrollTop();
+      wScrollDiff = wScrollBefore - wScrollCurrent;
+      elTop = parseInt($($element[i]).css('top')) + wScrollDiff;
+  
+      if (wScrollCurrent <= 0) {
+        $($element[i]).css('top', 0).removeClass('is-fixed is-hide');
+      } else if (wScrollDiff > 0) {
+        $($element[i]).css('top', 0).removeClass('is-hide').addClass('is-fixed');
+      } else if (wScrollDiff < 0) {
+        if (wScrollCurrent + wHeight >= dHeight - elHeight) {
+          // $($element[i]).css('top', (elTop = wScrollCurrent + wHeight - dHeight) < 0 ? elTop : 0);
+        } else {
+          $($element[i]).css('top', -($element.outerHeight(true))).addClass('is-hide');
         }
-      
       }
     
     }
